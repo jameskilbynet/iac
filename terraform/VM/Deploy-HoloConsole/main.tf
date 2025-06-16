ed44446cc12be20327d09e5afa0a4fa9fe1813a7
James Kilby <james@jameskilby.net>
2025-06-16 22:15:50 +0100

    Update provider

--- terraform/VM/Deploy-HoloConsole/main.tf
+++ terraform/VM/Deploy-HoloConsole/main.tf
@@ -1,8 +1,18 @@
-provider "vsphere" {
-  user           = var.vsphere_user
-  password       = var.vsphere_password
-  vsphere_server = var.vsphere_server
+terraform {
+  required_providers {
+    vsphere = {
+      source  = "vmware/vsphere"
+      version = ">= 2.2.0"
+    }
+  }
+  required_version = ">= 1.0.0"
+}
 
+# Configure the vSphere Provider
+provider "vsphere" {
+  user                 = var.vsphere_user
+  password             = var.vsphere_password
+  vsphere_server       = var.vsphere_server
   allow_unverified_ssl = true
 }
 
