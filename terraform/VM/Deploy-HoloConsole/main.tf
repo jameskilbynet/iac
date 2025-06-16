e905e4e03e3cd672255ac2973966b5acc81d3736
James Kilby <james@jameskilby.net>
2025-06-16 09:08:59 +0100

    Init of HoloConsole

---
+++ terraform/VM/Deploy-HoloConsole/main.tf
@@ -0,0 +1,70 @@
+provider "vsphere" {
+  user           = var.vsphere_user
+  password       = var.vsphere_password
+  vsphere_server = var.vsphere_server
+
+  allow_unverified_ssl = true
+}
+
+data "vsphere_datacenter" "dc" {
+  name = var.datacenter
+}
+
+data "vsphere_datastore" "datastore" {
+  name          = var.datastore
+  datacenter_id = data.vsphere_datacenter.dc.id
+}
+
+data "vsphere_compute_cluster" "cluster" {
+  name          = var.cluster
+  datacenter_id = data.vsphere_datacenter.dc.id
+}
+
+data "vsphere_network" "network" {
+  name          = var.network
+  datacenter_id = data.vsphere_datacenter.dc.id
+}
+
+data "vsphere_folder" "vm_folder" {
+  path          = var.vm_folder
+  datacenter_id = data.vsphere_datacenter.dc.id
+}
+
+resource "vsphere_virtual_machine" "vm_from_iso" {
+  name             = var.vm_name
+  resource_pool_id = data.vsphere_compute_cluster.cluster.resource_pool_id
+  datastore_id     = data.vsphere_datastore.datastore.id
+  folder           = data.vsphere_folder.vm_folder.path
+
+  num_cpus = 2
+  memory   = 8192
+  guest_id = "windows9Server64Guest" # or appropriate OS ID
+
+  network_interface {
+    network_id   = data.vsphere_network.network.id
+    adapter_type = "vmxnet3"
+  }
+
+  disk {
+    label            = "disk0"
+    size             = 60
+    eagerly_scrub    = false
+    thin_provisioned = true
+  }
+
+  cdrom {
+    datastore_id = data.vsphere_datastore.datastore.id
+    path         = var.iso_path
+  }
+
+  boot_options {
+    enter_bios_setup = false
+    boot_order       = ["cdrom", "disk"]
+  }
+
+  clone {
+    template_uuid = null # Required to use ISO instead of cloning
+  }
+
+  wait_for_guest_net_timeout = 0
+}
---
+++ terraform/VM/Deploy-HoloConsole/variables.tf
@@ -0,0 +1,10 @@
+variable "vsphere_user" {}
+variable "vsphere_password" {}
+variable "vsphere_server" {}
+variable "datacenter" {}
+variable "datastore" {}
+variable "cluster" {}
+variable "network" {}
+variable "vm_folder" {}
+variable "vm_name" {}
+variable "iso_path" {}
