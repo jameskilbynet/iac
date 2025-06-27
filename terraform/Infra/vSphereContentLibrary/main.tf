4729cb8f2cf962589b2b7b869c4b7e2847e4f038
James Kilby <james@jameskilby.net>
2025-06-27 11:59:28 +0100

    Update Provider

--- terraform/Infra/vSphereContentLibrary/main.tf
+++ terraform/Infra/vSphereContentLibrary/main.tf
@@ -8,6 +8,20 @@ terraform {
   required_version = ">= 1.0.0"
 }
 
+provider "vsphere" {
+  user           = var.vsphere_user
+  password       = var.vsphere_password
+  vsphere_server = var.vsphere_server
+
+  # If you're using a self-signed cert
+  allow_unverified_ssl = true
+}
+
+# Define these variables or pass them via a tfvars file or CLI
+variable "vsphere_user" {}
+variable "vsphere_password" {}
+variable "vsphere_server" {}
+
 data "vsphere_datacenter" "datacenter_a" {
   name = "uk-bhr-p-dc-1"
 }
@@ -35,12 +49,5 @@ resource "vsphere_content_library" "publisher_content_library2" {
   }
 }
 
-resource "vsphere_content_library" "publisher_content_library3" {
-  name            = "vmclogs"
-  description     = "vmclogs"
-  storage_backing = [data.vsphere_datastore.publisher_datastore.id]
-  publication {
-    published = true
-  }
-}
+
 
